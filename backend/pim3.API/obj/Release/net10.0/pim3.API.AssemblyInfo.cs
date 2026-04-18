b98f2d106a69b4a9411a56fbe1d9fc09bee6d03c
marcioaraujo7silva-cell <marcio.araujo7silva@gmail.com>
2026-04-18 15:01:24 -0300

    Mudança de nome

--- backend/pim3.API/obj/Release/net10.0/pim3.API.AssemblyInfo.cs
+++ backend/pim3.API/obj/Release/net10.0/pim3.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("pim3.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5e38a1c4377e64a69ffcf6cc2a9e5ed3bcaf75")]
 [assembly: System.Reflection.AssemblyProductAttribute("pim3.API")]
